ed322bf67abe8b355afcc398f550565676c1d177
Ankit Wosti <ankitwosti2@gmail.com>
2026-07-12 16:27:43 +0000

    server

---
+++ notes-api/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import notesRouter from './routes/notes.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express,json());
+app.use('/api/notes', notesRouter);
+
+app.get('/',(req,res)=>{
+    res.send('Notes api is running');
+});
+
+app.listen(port, ()=>{
+    console.log('serer running on http://localhost:${PORT}');
+});
