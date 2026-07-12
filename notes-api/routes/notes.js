71b30e5ccbb299b30b07e996d508a57b838bdb26
Ankit Wosti <ankitwosti2@gmail.com>
2026-07-12 16:20:58 +0000

    put delete added

--- notes-api/routes/notes.js
+++ notes-api/routes/notes.js
@@ -24,3 +24,22 @@ router.post('/',(req,res)=>{
     notes.push(newNote);
     res.status[200].json(newNote);
 });
+router.put('/:id',(req,res)=>{
+    const note = notes.find(n => n.id === Number(req.params.id));
+    if(!note){
+        return res.status(404).json({error:'note not found'});
+    }
+    const {title, content} = req.body;
+    if (title) note.title=title;
+    if(content) note.content=content;
+    res.json(note);
+});
+router.delete('/:id',(req,res)=>{
+    const index = notes.findIndex(n => n.id === Number(req.params.id));
+    if(index === -1){
+        return res.status(404).json({error: 'note not found'});
+    }
+    notes.splice(index,1);
+    res.status(204).send();
+})
+export default router;
