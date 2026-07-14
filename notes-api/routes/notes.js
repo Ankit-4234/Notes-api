a7586094c8448307eee680a6e3b2940ab2360048
Ankit Wosti <ankitwosti2@gmail.com>
2026-07-14 11:09:34 +0000

    modifications and improvised

--- notes-api/routes/notes.js
+++ notes-api/routes/notes.js
@@ -1,31 +1,27 @@
 import express from 'express';
+import note from '../modules/note.js';
+import note from '../modules/note.js';
 const router = express.Router();
 
-let notes = [
-    { id :1 , title : 'first note' , content:'this is my first note'}
-];
-let nextId = 2;
-router.get('/', (req,res) =>{
+router.get('/',async(req,res)=>{
+    const notes= await Note.find();
     res.json(notes);
 });
-router.get('/:id',(req,res)=>{
-    const note= notes.find(n=> n.id === Number(req.params.id));
-    if(!note){
-        return res.status(404).json({error: 'note not found'});
-    }
+router.get('/:id',async(req,res)=>{
+    const note = await Note.findById(req.params.id);
+    if(!note) return res.status(404).json({error:'note not found'});
     res.json(note);
 });
-router.post('/',(req,res)=>{
+router.post('/',async(req,res)=>{
     const {title, content}=req.body;
     if(!title || !content){
         return res.status(400).json({error: 'Title and content are required'});
     }
-    const newNote = {id:nextId++, title, content};
-    notes.push(newNote);
-    res.status(200).json(newNote);
+    const newNote = await Note.create({title, content});    
+    res.status(201).json(newNote);
 });
-router.put('/:id',(req,res)=>{
-    const note = notes.find(n => n.id === Number(req.params.id));
+router.put('/:id',async(req,res)=>{
+    const note= await Note.findById(req.pararms.id);
     if(!note){
         return res.status(404).json({error:'note not found'});
     }
@@ -34,9 +30,9 @@ router.put('/:id',(req,res)=>{
     if(content) note.content=content;
     res.json(note);
 });
-router.delete('/:id',(req,res)=>{
-    const index = notes.findIndex(n => n.id === Number(req.params.id));
-    if(index === -1){
+router.delete('/:id',async(req,res)=>{
+    const note = await note.findByIdAndDelete(req.params.id);
+    if(!note){
         return res.status(404).json({error: 'note not found'});
     }
     notes.splice(index,1);
--- notes-api/server.js
+++ notes-api/server.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import notesRouter from './routes/notes.js';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -11,6 +15,13 @@ app.get('/',(req,res)=>{
     res.send('Notes api is running');
 });
 
-app.listen(port, ()=>{
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log('connected to mongodb');
+    app.listen(port, ()=>{
     console.log(`serer running on http://localhost:${port}`);
+});
+})
+.catch((err)=>{
+    console.log('MongoDB connection error',err);
 });
